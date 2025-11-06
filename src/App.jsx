7dc2c077db98f5b7c828d134c883f5f20d1789d7
nothing36 <prodipsarker953@gmail.com>
2025-11-06 11:19:13 +0600

    click event added

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  function handleClick() {
+    alert("Button clicked");
+  }
+  const handleAdd = (x) => {
+  
+  alert (`Added ${x} sum is ${x+5}`)
+  };
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Hi this is react concept.</h1>
+      <button className="btn" onClick={handleClick}>
+        Click here
+      </button>
+      <button onClick={() => alert("another button is clicked")}>
+        Click me
+      </button>
+      <button onClick={() => handleAdd(7)}>Add</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
