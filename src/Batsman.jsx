d5ba56fcd1718bfdd72c07e02825e8bce8cd792a
nothing36 <prodipsarker953@gmail.com>
2025-11-09 10:52:53 +0600

    use state for batsman

--- src/Batsman.jsx
+++ src/Batsman.jsx
@@ -1,8 +1,39 @@
-export default function Batsman(){
+import { useState } from "react";
 
-return(
+export default function Batsman() {
+  const [runs, setRuns] = useState(0);
+  const [fours, setFours] = useState(0);
+  const [sixes, setSixes] = useState(0);
+  const handleSingle = () => {
+    const updatedRuns = runs + 1;
+    setRuns(updatedRuns);
+  };
+  const handleFours = () => {
+    const updatedRuns = runs + 4;
+    setRuns(updatedRuns);
+    setFours(fours + 1);
+  };
+
+  const handleSix = () => {
+    const updatedRuns = runs + 6;
+    setRuns(updatedRuns);
+    setSixes(sixes + 1);
+  };
+
+  return (
     <div>
-        <h2>Score Board</h2>
+      <h2>Score Board</h2>
+      <h3>Player: Rohit Sharma</h3>
+      {runs >= 50 && <h2>You have made a half century.</h2>}
+      {runs >= 100 && (
+        <h2>Congratulations!! You have reached golden three figures..</h2>
+      )}
+      <h4>Score: {runs}</h4>
+      <h5>Fours: {fours}</h5>
+      <h5>Sixes: {sixes}</h5>
+      <button onClick={handleSingle}>Single</button>
+      <button onClick={handleFours}>Four</button>
+      <button onClick={handleSix}>Six</button>
     </div>
-)
+  );
 }
